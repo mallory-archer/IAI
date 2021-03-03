26f16094672f7f00ec9996e377beb3dbafc86793
Grace Haaf <gracehaaf@Graces-MacBook-Air.local>
2021-03-03 22:04:06 +0800

    coded RUM mod model

--- random_utility_model.py
+++ random_utility_model.py
@@ -3,6 +3,7 @@ import os
 import json
 # import pandas as pd
 import numpy as np
+from scipy.optimize import minimize
 
 from assumption_calc_functions import calc_prob_winning_slansky_rank
 from assumption_calc_functions import create_game_hand_index
@@ -42,18 +43,6 @@ class Option:
         self.name = name
         self.outcomes = outcomes
 
-    # def calc_option_value(self, alpha_f, lambda_f, beta_f, gamma_f, delta_f):
-    #     return sum([calc_outcome_value(
-    #         calc_outcome_v(x_f=t_outcome['payoff'], alpha_f=alpha_f, lambda_f=lambda_f, beta_f=beta_f),
-    #         calc_outcome_pi(p_f=t_outcome['prob'], c_f=gamma_f if (t_outcome['payoff'] >= 0) else delta_f))
-    #                 for _, t_outcome in self.outcomes.items()])
-    #
-    # # def calc_option_utility(self):
-    # #     return sum([calc_outcome_value(v_f=t_outcome['payoff'], pi_f=t_outcome['prob']) for _, t_outcome in self.outcomes.items()])
-    #
-    # def calc_option_utility(self, lose_weight_f=1):
-    #     return sum([calc_outcome_value(v_f=t_outcome['payoff'], pi_f=t_outcome['prob']) * (lose_weight_f if t_name == 'lose' else 1) for t_name, t_outcome in self.outcomes.items()])
-
 
 class ChoiceSituation:
     def __init__(self, sit_options, sit_choice=None, slansky_strength=None, stack_rank=None, seat=None, post_loss=None):
@@ -64,74 +53,10 @@ class ChoiceSituation:
         self.stack_rank = stack_rank
         self.seat = seat
         self.post_loss = post_loss
-        # self.values = None
-        # self.prob_options = None
-        # self.prob_choice = None
-        # self.pred_choice = None
-        # self.util_values = None
-        # self.util_prob_options = None
-        # self.util_prob_choice = None
-        # if (self.choice is not None) and self.choice not in self.option_names:
-        #     print("Warning: specified choice of '%s' is not an option in choice set: %s" % (
-        #     self.choice, self.option_names))
 
     def print(self):
         for k, v in self.__dict__.items():
             print('%s: %s' % (k, v))
-
-    # prospect model formulation
-    # def get_option_values(self, alpha_f, lambda_f, beta_f, gamma_f, delta_f):
-    #     return [x.calc_option_value(alpha_f, lambda_f, beta_f, gamma_f, delta_f) for x in self.options]
-    #
-    # def get_option_probs(self, phi_f):
-    #     if self.values is not None:
-    #         return calc_prob_options(self.values, phi_f)
-    #     else:
-    #         print("Error: no option values stored for choice situation. Set via method 'get_option_values(args)' first")
-    #         return None
-    #
-    # def get_choice_location(self, choice_f):
-    #     return self.option_names.index(choice_f)
-    #
-    # def get_prob_choice(self, choice_f):
-    #     if self.prob_options is not None:
-    #         return self.prob_options[self.get_choice_location(choice_f)]
-    #     else:
-    #         print(
-    #             "Error: no option probabilities stored for choice situation. Set via method 'get_option_probs(args)' first")
-    #         return None
-    #
-    # def set_model_values(self, alpha_f, lambda_f, beta_f, gamma_f, delta_f, phi_f):
-    #     self.values = self.get_option_values(alpha_f, lambda_f, beta_f, gamma_f, delta_f)
-    #     self.prob_options = self.get_option_probs(phi_f)
-    #     self.prob_choice = self.get_prob_choice(self.choice)
-    #
-    # def predict_choice(self):
-    #     self.pred_choice = self.option_names[self.prob_options.index(max(self.prob_options))]
-    #
-    # # traditional utility model formulation
-    # def get_util_values(self, lose_weight_f):
-    #     return [x.calc_option_utility(lose_weight_f) for x in self.options]
-    #
-    # def get_util_option_probs(self, phi_f):
-    #     if self.util_values is not None:
-    #         return calc_prob_options(self.util_values, phi_f)
-    #     else:
-    #         print("Error: no utility values stored for choice situation. Set via method 'get_option_values(args)' first")
-    #         return None
-    #
-    # def get_util_prob_choice(self, choice_f):
-    #     if self.util_prob_options is not None:
-    #         return self.util_prob_options[self.get_choice_location(choice_f)]
-    #     else:
-    #         print(
-    #             "Error: no option probabilities stored for choice situation. Set via method 'get_option_probs(args)' first")
-    #         return None
-    #
-    # def set_util_model_values(self, lose_weight_f, phi_f):
-    #     self.util_values = self.get_util_values(lose_weight_f)
-    #     self.util_prob_options = self.get_util_option_probs(phi_f)
-    #     self.util_prob_choice = self.get_util_prob_choice(self.choice)
 
 
 def generate_choice_situations(player_f, game_hand_index_f, prob_dict_f, payoff_dict_f):
@@ -188,11 +113,7 @@ def generate_choice_situations(player_f, game_hand_index_f, prob_dict_f, payoff_
 
 def reformat_choice_situations_for_model(choice_situations):
     # create dictionary of option params
-    unique_slansky = set([cs.slansky_strength for cs in choice_situations])
-    unique_seat = set([cs.seat for cs in choice_situations])
-    choice_param_dictionary = dict(zip(unique_slansky, [
-        dict(zip(unique_seat, [{'params': dict(), 'n_chosen': {'play': 0, 'fold': 0}}] * len(unique_seat)))] * len(
-        unique_slansky)))
+    choice_param_dictionary = {rank: {seat: {'params': dict(), 'n_chosen': {'play': 0, 'fold': 0}} for seat in set([cs.seat for cs in choice_situations])} for rank in set([cs.slansky_strength for cs in choice_situations])}
     for cs in choice_situations:
         for i in range(len(cs.option_names)):
             choice_param_dictionary[cs.slansky_strength][cs.seat]['params'].update(
@@ -206,31 +127,130 @@ def calc_CRRA_utility(outcomes, omega):
         if payoff == 0:
             return 0
         else:
-            return prob * (payoff ** (1 - omega)) / (1 - omega)
-    return sum([calc_outcome_util(o['prob'], o['payoff'], omega) for o in outcomes])
+            if omega == 1:
+                return np.log(payoff)
+            else:
+                return prob * (np.sign(payoff) * (abs(payoff) ** (1 - omega))) / (1 - omega)
+    return sum([calc_outcome_util(prob=o['prob'], payoff=o['payoff'], omega=omega) for o in outcomes])
 
-
-def calc_RUM_LL(kappa, lam, omega, data):
-    def calc_LLi(X, Y, I, util_X, util_Y, kappa, lam):
-        return (X + I/2) * np.log(((1-2*kappa)*np.exp(lam*util_X)) / (np.exp(lam*util_X + np.exp(lam*util_Y))) + kappa) + (Y + I / 2) * np.log(((1 - 2 * kappa) * np.exp(lam * util_Y)) / (np.exp(lam * util_X + np.exp(lam * util_Y))) + kappa)
-    LLi = list()
-    for rank in data.keys():
-        for seat in data[rank].keys():
-            Xi = data[rank][seat]['n_chosen']['play']
-            Yi = data[rank][seat]['n_chosen']['fold']
-            Ii = 0
-            util_Xi = calc_CRRA_utility(data[rank][seat]['params']['play'], omega)
-            util_Yi = calc_CRRA_utility(data[rank][seat]['params']['fold'], omega)
-            LLi.append(calc_LLi(Xi, Yi, Ii, util_Xi, util_Yi, kappa, lam))
-    return sum(LLi)
+# def calc_RUM_LL(kappa, lam, omega, data):
+#     def calc_LLi(X, Y, I, util_X, util_Y, kappa, lam):
+#         return (X + I/2) * np.log(((1-2*kappa)*np.exp(lam*util_X)) / (np.exp(lam*util_X) + np.exp(lam*util_Y)) + kappa) + \
+#                (Y + I / 2) * np.log(((1 - 2 * kappa) * np.exp(lam * util_Y)) / (np.exp(lam * util_X) + np.exp(lam * util_Y)) + kappa)
+#     LLi = list()
+#     for rank in data.keys():
+#         for seat in data[rank].keys():
+#             # Xi = data[rank][seat]['n_chosen']['play']
+#             # Yi = data[rank][seat]['n_chosen']['fold']
+#             # Ii = 0
+#             # util_Xi = calc_CRRA_utility(data[rank][seat]['params']['play'], omega)
+#             # util_Yi = calc_CRRA_utility(data[rank][seat]['params']['fold'], omega)
+#             LLi.append(calc_LLi(X=data[rank][seat]['n_chosen']['play'],
+#                                 Y=data[rank][seat]['n_chosen']['fold'],
+#                                 I=0,
+#                                 util_X=calc_CRRA_utility(data[rank][seat]['params']['play'], omega),
+#                                 util_Y=calc_CRRA_utility(data[rank][seat]['params']['fold'], omega),
+#                                 kappa=kappa,
+#                                 lam=lam))
+#     return sum(LLi)
 
 
 # Run code
+
 choice_situations = generate_choice_situations(player_f=players[select_player_index], game_hand_index_f=game_hand_player_index, payoff_dict_f=payoff_dict, prob_dict_f=prob_dict)
 
 choice_param_dictionary = reformat_choice_situations_for_model(choice_situations)
 
-LL_RUM = calc_RUM_LL(kappa=0.008, lam=4.572, omega=2, data=choice_param_dictionary)
+# LL_RUM = calc_RUM_LL(kappa=0.008, lam=4.572, omega=2, data=choice_param_dictionary)
+
+
+class RandomUtilityModel:
+    def __init__(self, data_f):
+        self.data_f = data_f
+        self.kappa_f = None
+        self.lambda_f = None
+        self.omega_f = None
+        self.param_names_f = ['kappa', 'lambda', 'omega']
+        self.init_params = None
+        self.results = None
+
+    # ---- Manual LL and score ----
+        # replaces default loglikelihood for each observation
+    def negLL(self, params):
+        def calc_LLi(X, Y, I, util_X, util_Y, kappa, lam):
+            return (X + I / 2) * np.log(((1 - 2 * kappa) * np.exp(lam * util_X)) / (
+                    np.exp(lam * util_X) + np.exp(lam * util_Y)) + kappa) + \
+                   (Y + I / 2) * np.log(((1 - 2 * kappa) * np.exp(lam * util_Y)) / (
+                    np.exp(lam * util_X) + np.exp(lam * util_Y)) + kappa)
+
+        self.kappa_f = params[self.param_names_f.index('kappa')]
+        self.lambda_f = params[self.param_names_f.index('lambda')]
+        self.omega_f = params[self.param_names_f.index('omega')]
+
+        LLi = list()
+        for rank in self.data_f.keys():
+            for seat in self.data_f[rank].keys():
+                LLi.append(calc_LLi(X=self.data_f[rank][seat]['n_chosen']['play'],
+                                    Y=self.data_f[rank][seat]['n_chosen']['fold'],
+                                    I=0,
+                                    util_X=calc_CRRA_utility(outcomes=self.data_f[rank][seat]['params']['play'], omega=self.omega_f),
+                                    util_Y=calc_CRRA_utility(outcomes=self.data_f[rank][seat]['params']['fold'], omega=self.omega_f),
+                                    kappa=self.kappa_f,
+                                    lam=self.lambda_f))
+
+        return -sum(LLi)*10
+
+    def fit(self, init_params=None):
+        if init_params is None:
+            self.init_params = [1] * len(self.param_names_f)
+        else:
+            self.init_params = init_params
+
+        self.results = minimize(self.negLL, self.init_params, method="Nelder-Mead", options={"disp": True})
+
+    def print(self):
+        for k, v in self.__dict__.items():
+            print('%s: %s' % (k, v))
+
+import copy
+test_dict = copy.deepcopy(choice_param_dictionary)
+# calc_RUM_LL(kappa=0.008, lam=4.572, omega=2, data=test_dict)
+test_dict.pop(9)
+test_dict[5].pop(4)
+
+test = RandomUtilityModel(choice_param_dictionary)
+test.negLL([0.008, 4.5, 0.5])
+test.fit(init_params=[0.008, 5, 0.5])
+test.print()
+
+import matplotlib.pyplot as plt
+def test_CRRA_calc(data, x):
+    try:
+        return calc_CRRA_utility(data, x)
+    except ZeroDivisionError:
+        return 100
+def test_negLL_calc(model_class, x):
+    try:
+        model_class.negLL([0.008, 4.5, x])
+    except ZeroDivisionError:
+        return 100
+
+
+plt.scatter([x for x in [i/100 for i in range(1, 300)]], [test_CRRA_calc(choice_param_dictionary[5][6]['params']['play'], x) for x in [i/100 for i in range(1, 300)]])
+
+def calc_prob(outcomes_x, outcomes_y, lambda_f, omega_f):
+    Ux = test_CRRA_calc(data=outcomes_x, x=omega_f)
+    Uy = test_CRRA_calc(data=outcomes_y, x=omega_f)
+    return np.exp(lambda_f * Ux) / (np.exp(lambda_f * Ux) + np.exp(lambda_f * Uy))
+outcomes_x = [{'payoff': -1, 'prob': 0.9}, {'payoff': 100, 'prob': 0.1}]
+outcomes_y = [{'payoff': 0, 'prob': 1}]
+calc_CRRA_utility(outcomes=outcomes_x, omega=0.4)
+plt.scatter([x for x in [i/100 for i in range(0, 100)]], [test_CRRA_calc(data=outcomes_x, x=x) for x in [i/100 for i in range(0, 100)]])
+plt.scatter([x for x in [i/100 for i in range(0, 100)]], [test_CRRA_calc(data=outcomes_y, x=x) for x in [i/100 for i in range(0, 100)]])
+plt.scatter([x for x in [i/100 for i in range(1, 100)]], [test_CRRA_calc(data=outcomes_x, x=x) - test_CRRA_calc(data=outcomes_y, x=x) for x in [i/100 for i in range(1, 100)]])
+plt.scatter([x for x in [i/100 for i in range(1, 100)]], [calc_prob(outcomes_x, outcomes_y, lambda_f=1, omega_f=x) for x in [i/100 for i in range(1, 100)]])
+
+plt.scatter([x for x in [i/100 for i in range(1, 300)]], [test_negLL_calc(test, x) for x in [i/100 for i in range(1, 300)]])
 
 # ---------- ARCHIVE -------
 # data frame for sanity checking / working
